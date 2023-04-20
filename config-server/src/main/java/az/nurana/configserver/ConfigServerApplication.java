8eb61e91156f760e0c6289a4259786091c67e90f
Nunik95 <95641980+Nunik95@users.noreply.github.com>
2023-04-20 17:51:37 +0400

    upds

--- config-server/src/main/java/az/nurana/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/az/nurana/configserver/ConfigServerApplication.java
@@ -2,9 +2,10 @@ package az.nurana.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
